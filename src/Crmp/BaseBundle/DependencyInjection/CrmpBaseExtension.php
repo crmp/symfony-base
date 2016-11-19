d977babb42fff3aadfa894884e306d86b6e081dd
Mike Pretzlaw <pretzlaw@gmail.com>
2016-11-19 19:35:09 +0100

    Assetic: SCSS can be build now.
    The new filter "scssphp" builds the given scss
    and serves the css to the browser.

--- src/Crmp/BaseBundle/DependencyInjection/CrmpBaseExtension.php
+++ src/Crmp/BaseBundle/DependencyInjection/CrmpBaseExtension.php
@@ -4,6 +4,7 @@ namespace Crmp\BaseBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class CrmpBaseExtension extends Extension
+class CrmpBaseExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +25,16 @@ class CrmpBaseExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    /**
+     * Prepend configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $ymlLoader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $ymlLoader->load('config.yml');
     }
 }
